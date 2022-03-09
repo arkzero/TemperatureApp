eba2e527b5c46a4843d91a161b767a701f297e09
Bryan Nissen <bryannissen@gmail.com>
2022-03-09 18:33:15 -0500

    Update Searchbar

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipCode: props.zipCode,
+    };
+  }
+
   render() {
     return (
-      <form>
+      <form style={{ textAlign: 'center' }}>
         <label htmlFor="zipCode">
           Enter Zip Code
           <input
@@ -15,5 +24,13 @@ class SearchBar extends React.Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  zipCode: PropTypes.string,
+};
+
+SearchBar.defaultProps = {
+  zipCode: '00000',
+};
 
 export default SearchBar;
